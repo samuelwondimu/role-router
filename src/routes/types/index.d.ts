baf614e830ff1198dd24af889c366ebd42e25917
samuelwondimu <wsamuelwondimu@gmail.com>
2021-08-31 13:33:46 +0300

    types for routes and children routes

---
+++ src/routes/types/index.d.ts
@@ -0,0 +1,18 @@
+import { ComponentType, LazyExoticComponent, ReactNode } from "react";
+
+export interface childrenRouter {
+  title: string;
+  path: string;
+  exact?: boolean;
+  component: LazyExoticComponent<ComponentType<any>>;
+  permissions?: string[];
+}
+export interface RouterConfig {
+  title: string;
+  path: string;
+  exact?: boolean;
+  component?: LazyExoticComponent<ComponentType<any>>;
+  routes?: RouterConfig[];
+  children?: childrenRouter[];
+  permissions?: string[];
+}
